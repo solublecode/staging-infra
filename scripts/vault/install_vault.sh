1fbb79a69c269e52667283143fc2ffdbfc959d1f
Collin Mutembei <mutembei.collin@gmail.com>
2020-08-29 01:40:21 +0300

    Setup config directories in cloud-init

--- scripts/vault/install_vault.sh
+++ scripts/vault/install_vault.sh
@@ -7,7 +7,6 @@ export VAULT_ADDR=http://127.0.0.1:8200
 wget https://releases.hashicorp.com/vault/1.5.2/vault_1.5.2_linux_amd64.zip
 unzip vault_1.5.2_linux_amd64.zip -d /usr/local/bin
 
-mkdir -p /root/vault
 # Start vault as a service
 systemctl enable vault-server.service
 systemctl start vault-server.service
